313b1802ec255521e0ea854b692f96d31eac825e
Zhang020101 <99615093+Zhang020101@users.noreply.github.com>
2023-06-22 15:43:05 +0300

    Create source file teacher.c

---
+++ src/teacher.c
@@ -0,0 +1,29 @@
+#include <stdio.h> 
+#include "teacher.h"
+
+void steacher (void)
+{
+
+struct teacherData teacher;
+ 
+teacher.teacher_name = "Maryia"; 
+teacher.teacher_id = 123456; 
+teacher.teacher_age = 45;
+
+teacher.teacher_Address.street = "4, pr. Nezavisimosti"; 
+teacher.teacher_Address.state = "Minskaya";
+ teacher.teacher_Address.city = "Minsk";
+ 
+
+printf ("Hello, %s!", teacher.teacher_name); 
+printf("\nTeacher Id is: %d", teacher.teacher_id); 
+printf("\nTeacher Age is: %d", teacher.teacher_age);
+printf("\n Regin %s, City %s, Address %s", 
+	
+	teacher.teacher_Address.state, 
+	teacher.teacher_Address.city, 
+	teacher.teacher_Address.street);
+printf ("\nGoodbye!"); 
+return 0;
+}
+
